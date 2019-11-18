9aa2dfd0114c4c8e27eac1a4f0155723c387993c
PaweuB <spinacz@gmail.com>
2019-11-18 14:04:13 +0100

    Looping through Objects

---
+++ Looping through Objects/main.js
@@ -0,0 +1,31 @@
+let spaceship = {
+    crew: {
+    captain: { 
+        name: 'Lily', 
+        degree: 'Computer Engineering', 
+        cheerTeam() { console.log('You got this!') } 
+        },
+    'chief officer': { 
+        name: 'Dan', 
+        degree: 'Aerospace Engineering', 
+        agree() { console.log('I agree, captain!') } 
+        },
+    medic: { 
+        name: 'Clementine', 
+        degree: 'Physics', 
+        announce() { console.log(`Jets on!`) } },
+    translator: {
+        name: 'Shauna', 
+        degree: 'Conservation Science', 
+        powerFuel() { console.log('The tank is full!') } 
+        }
+    }
+}; 
+
+for (let ele in spaceship.crew) {
+  console.log(`${ele}: ${spaceship.crew[ele].name}`);
+}
+
+for (let ele in spaceship.crew) {
+  console.log(`${spaceship.crew[ele].name}: ${spaceship.crew[ele].degree}`);
+}
